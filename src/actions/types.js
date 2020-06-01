724badc2e80c53f2840449b8620da403623c51ad
domcattell <55451364+domcattell@users.noreply.github.com>
2020-06-01 17:03:21 +0100

    initial setup

---
+++ src/actions/types.js
@@ -0,0 +1,13 @@
+//get 
+export const GET_USERS = "GET_USERS"
+export const GET_POSTS = "GET_POSTS"
+export const GET_COMMENTS = "GET_COMMENTS"
+
+//post
+export const ADD_POST = "ADD_POST"
+
+//put
+export const EDIT_POST = "EDIT_POST"
+
+//delete
+export const DELETE_POST = "DELETE_POST"
