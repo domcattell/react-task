9c27e04511f45a19c3ec4e72f33e78f51cb4ebc6
domcattell <55451364+domcattell@users.noreply.github.com>
2020-06-07 14:32:10 +0100

    Updates

---
+++ precache-manifest.7d8b61b1772218788af41d4491e030bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3b9814961568d863fa98ca235170b60",
+    "url": "/react-task/index.html"
+  },
+  {
+    "revision": "33e024011d7d95795283",
+    "url": "/react-task/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2684cd368228eb4f6c68",
+    "url": "/react-task/static/css/main.647c5a8d.chunk.css"
+  },
+  {
+    "revision": "33e024011d7d95795283",
+    "url": "/react-task/static/js/2.3b269b46.chunk.js"
+  },
+  {
+    "revision": "8bb757413c38ce564bf5d6ba97eb4669",
+    "url": "/react-task/static/js/2.3b269b46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2684cd368228eb4f6c68",
+    "url": "/react-task/static/js/main.b4006e0e.chunk.js"
+  },
+  {
+    "revision": "a66ed87e1e53d8a73f4e",
+    "url": "/react-task/static/js/runtime-main.185d6e69.js"
+  }
+]);
